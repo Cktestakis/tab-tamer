f01223a2641fc6ad7ec4e76878dee2db10ac75f4
Cktestakis <codyktestakis@gmail.com>
2024-10-15 16:17:09 -0400

    amount of tabs opened decreased.

--- background.js
+++ background.js
@@ -1,13 +1,12 @@
 chrome.tabs.onCreated.addListener(() => {
-    chrome.tabs.query({}, (tabs) => {
-      if (tabs.length > 20) {
-        chrome.notifications.create({
-          type: "basic",
-          iconUrl: "icon.png",
-          title: "Tab Tamer Alert",
-          message: "You have more than 20 tabs open!"
-        });
-      }
-    });
+  chrome.tabs.query({}, (tabs) => {
+    if (tabs.length > 10) {
+      chrome.notifications.create({
+        type: "basic",
+        iconUrl: "icon.png",
+        title: "Tab Tamer Alert",
+        message: "You have more than 20 tabs open!",
+      });
+    }
   });
-  
+});
